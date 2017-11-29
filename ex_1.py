566f24d285c36410163bd01069aba2c886d51a63
Yavlados <31962780+Yavlados@users.noreply.github.com>
2017-11-29 17:57:12 +0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from librip.gens import field, gen_random
+
+goods = [
+    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
+    {'title': 'Шкаф', 'price': 20500, 'color': 'red'},
+    {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
+]
+
+# Реализация задания 1
+print(*('\'' + x + '\'' for x in field(goods, 'title')))
+print(*(x for x in field(goods, 'title', 'price')))
+print(*(x for x in gen_random(1, 3, 5)))
---
+++ zadanie.py
@@ -0,0 +1,5 @@
+res = [x*x for x in range(1, 6, 1)]
+print (res)
+a = (1, 2, 3, 4, 5)
+value = list(map(lambda x: x * x, a))
+print (value)
